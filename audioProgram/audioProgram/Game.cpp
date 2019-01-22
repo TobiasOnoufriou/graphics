a4dc8461393ea516f451e74655ce67b785e2f073
Tobi <spacehomer@live.co.uk>
2019-01-22 01:33:35 +0000

    added so I can make a triangle
    Small steps make big beginnings

--- audioProgram/audioProgram/Game.cpp
+++ audioProgram/audioProgram/Game.cpp
@@ -4,6 +4,8 @@ Game::Game() {
 }
 
 Game::~Game() {
+	glDeleteVertexArrays(1, &vao);
+	glDeleteBuffers(1, &vbo);
 	glfwDestroyWindow(window);
 	glfwTerminate();
 }
@@ -14,9 +16,13 @@ void error_callback(int error, const char* description) {
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
 
 }
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
 void Game::opengl_setup(int width, int height) {
-	
-	
+
+
 	if (!glfwInit()) {
 		exit(EXIT_FAILURE);
 	}
@@ -26,27 +32,114 @@ void Game::opengl_setup(int width, int height) {
 		glfwTerminate();
 		exit(EXIT_FAILURE);
 	}
-	
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	glfwMakeContextCurrent(this->window);
-	
+
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		exit(EXIT_FAILURE);
 	}
+
+
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+
+	std::string vertexShaderSource;
+	std::string fragmentShaderSource;
+	if (!loadShaderSource("vertex.vs", vertexShaderSource)) {
+		std::cout << "Could not find vertex shader" << std::endl;
+	}
+	if (!loadShaderSource("fragment.fs", fragmentShaderSource)) {
+		std::cout << "Could not find fragment shader" << std::endl;
+	}
+	const char *vertexSource = vertexShaderSource.c_str();
+	glShaderSource(vertexShader, 1, &vertexSource , NULL);
+	glCompileShader(vertexShader);
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
 	
+	const char* fragmentSource = fragmentShaderSource.c_str();
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo);
+	
+	glBindVertexArray(vao);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+
 	//glfwSetKeyCallback(this->window, this->key_callback);
 }
+bool Game::loadShaderSource(const std::string& filename, std::string& out) {
+	std::ifstream file;
+	file.open(filename.c_str());
+	if (!file) {
+		return false;
+	}
+	std::stringstream stream;
+	stream << file.rdbuf();
+	file.close();
+	out = stream.str();
+	return true;
+}
 
+void Game::processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
 void Game::game_loop() {
-	glViewport(0, 0, 800, 600);
+	
+	this->processInput(window);
+
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
-	glfwSwapBuffers(this->window);
+	
+	
+	glUseProgram(shaderProgram);
+	
+	glBindVertexArray(vao);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 
-
+	
 	glfwSwapBuffers(window);
 	glfwPollEvents();
 }
--- audioProgram/audioProgram/Game.h
+++ audioProgram/audioProgram/Game.h
@@ -1,19 +1,28 @@
 #pragma once
 #include "glad/glad.h"
 #include "glfw/include/GLFW/glfw3.h"
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
+
 class Game {
 private:
 	GLFWwindow* window;
 	GLenum res;
 	GLuint vbo;
+	GLuint vao;
+	GLuint vertexShader;
+	GLuint fragmentShader;
+	GLuint shaderProgram;
 
 public:
 	Game();
 	~Game();
+	bool loadShaderSource(const std::string& filename, std::string& out);
 	void opengl_setup(int width, int height);
 	void error_callback(int error, const char* description);
-	void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+	void processInput(GLFWwindow* window);
 	void game_loop();
 
 	//Mutator
